9f02c4007d7e9fbad6b2debb483232925566da60
visual_community@outlook.com <visual_community@outlook.com>
2020-02-15 10:28:59 -0400

    Mi primer modelo

---
+++ soluno/webappuno/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webappuno.Models
+{
+    public class Person
+    {
+        [key]
+        public int PersonID { get; set; }
+
+        [ Required(ErrorMessage ="El nombre es requerido")]
+        [ Display(Name="Nombre completo")]
+        [ StringLength(100,ErrorMessage ="Maximo 100 caracteres")]
+        public string Name { get; set; }
+
+        [Display(Name="Carnet de Identidad")]
+        public string Document { get; set; }
+    }
+}
